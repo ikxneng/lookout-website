aa47dcce9be489c095fe0dd3f6b7b7fa0e47df36
ikxneng <97749494+ikxneng@users.noreply.github.com>
2025-02-13 14:03:20 +0200

    Update App.jsx

--- lookout/src/App.jsx
+++ lookout/src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import About from "./components/About"
+import Events from "./components/Events"
+import Gallery from './components/Gallery'
+import Services from './components/Services';
+import Footer from './components/Footer'
 
 
 function App() {
@@ -11,15 +14,23 @@ function App() {
 
   return (
 
-      <div> 
-        <NavBar />
-        <Home />
-       </div>
+    <Router>
+    <div className='flex flex-col min-h-screen'>
+      <NavBar />
+      <main className='flex-grow'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      </main>
+      <Footer />
+    </div>
+  </Router>
       
-
-      
-
-  )
-}
+  );
+};
 
 export default App
